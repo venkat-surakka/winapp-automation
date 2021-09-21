41eecb5f5555ec592e2dc89a437f90d693447c24
Venkat Surakka <venkat.surakka@outlook.com>
2021-09-21 22:55:52 +1200

    removed comments

--- src/test/java/functionalTests/calculator/XpathTests.java
+++ src/test/java/functionalTests/calculator/XpathTests.java
@@ -23,16 +23,9 @@ public class XpathTests extends TestBase {
         CalculatorSession.findElementByAccessibilityId("num5Button").click();
         CalculatorSession.findElementByAccessibilityId("plusButton").click();
         CalculatorSession.findElementByAccessibilityId("num9Button").click();
-//        CalculatorSession.findElementByAccessibilityId("equalButton").click();
         calculatorPO.clickEquals();
-
-//        CalculatorResult = CalculatorSession.findElementByAccessibilityId("CalculatorResults");
-//        Assertions.assertTrue(CalculatorResult.getText().contains("14"));
 
         String currentText = GenericFunctions.getCurrentResults(CalculatorSession);
         Assertions.assertTrue(currentText.contains("14"));
-
-        // Similarly for getCellValue it should be
-        // String cellValue = GenericFunctions.getCellValue(CalculatorSession, 2, 0);
     }
 }
